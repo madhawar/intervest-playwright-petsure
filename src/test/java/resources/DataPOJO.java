62883cf4dd0115db7bcb4593caacaa4401abd02d
Madhawa <madhawa@intervest.lk>
2021-06-30 07:25:46 +0530

    cleaned up the DataPOJO class as well

--- src/test/java/resources/DataPOJO.java
+++ src/test/java/resources/DataPOJO.java
@@ -1,100 +1,105 @@
 package resources;
 
 public class DataPOJO {
-    private String firstname;
-    public String getFirstName() {
-        return firstname;
-    }
+//    private String title;
+//    public String getTitle() {
+//        return title;
+//    }
 
-    private String lastname;
-    public String getLastName() {
-        return lastname;
-    }
-
-    private String bDay;
-    public String getOwnerBDay() {
-        return bDay;
-    }
-
-    private String bMonth;
-    public String getOwnerBMonth() {
-        return bMonth;
-    }
-
-    private String bYear;
-    public String getOwnerBYear() {
-        return bYear;
-    }
-
-    private String countrycode;
-    public String getCountryCode() {
-        return countrycode;
-    }
-
-    private String mobilenumber;
-    public String getMobileNumber() {
-        return mobilenumber;
-    }
-
-    private String email;
-    public String getEmailAddress() {
-        return email;
-    }
-
-    private String password;
-    public String getPassword() {
-        return password;
-    }
-
-    private String postcode;
-    public String getPostcode() {
-        return postcode;
-    }
-
-    private String address1;
-    public String getAddress1() {
-        return address1;
-    }
-
-    private String address2;
-    public String getAddress2() {
-        return address2;
-    }
-
-    private String city;
-    public String getCity() {
-        return city;
-    }
-
-    private String country;
-    public String getCountry() {
-        return country;
-    }
-
-    private String cardholder;
-    public String getCardHolder() {
-        return cardholder;
-    }
-
-    private String cardnumber;
-    public String getCardNumber() {
-        return cardnumber;
-    }
-
-    private String cardmonth;
-    public String getCardMonth() {
-        return cardmonth;
-    }
-
-    private String cardyear;
-    public String getCardYear() {
-        return cardyear;
-    }
-
-    private String cardcvv;
-    public String getCardCVV() {
-        return cardcvv;
-    }
+//    private String firstname;
+//    public String getFirstName() {
+//        return firstname;
+//    }
+//
+//    private String lastname;
+//    public String getLastName() {
+//        return lastname;
+//    }
+//
+//    private String bDay;
+//    public String getOwnerBDay() {
+//        return bDay;
+//    }
+//
+//    private String bMonth;
+//    public String getOwnerBMonth() {
+//        return bMonth;
+//    }
+//
+//    private String bYear;
+//    public String getOwnerBYear() {
+//        return bYear;
+//    }
+//
+//    private String countrycode;
+//    public String getCountryCode() {
+//        return countrycode;
+//    }
+//
+//    private String mobilenumber;
+//    public String getMobileNumber() {
+//        return mobilenumber;
+//    }
+//
+//    private String email;
+//    public String getEmailAddress() {
+//        return email;
+//    }
+//
+//    private String password;
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    private String postcode;
+//    public String getPostcode() {
+//        return postcode;
+//    }
+//
+//    private String address1;
+//    public String getAddress1() {
+//        return address1;
+//    }
+//
+//    private String address2;
+//    public String getAddress2() {
+//        return address2;
+//    }
+//
+//    private String city;
+//    public String getCity() {
+//        return city;
+//    }
+//
+//    private String country;
+//    public String getCountry() {
+//        return country;
+//    }
+//
+//    private String cardholder;
+//    public String getCardHolder() {
+//        return cardholder;
+//    }
+//
+//    private String cardnumber;
+//    public String getCardNumber() {
+//        return cardnumber;
+//    }
+//
+//    private String cardmonth;
+//    public String getCardMonth() {
+//        return cardmonth;
+//    }
+//
+//    private String cardyear;
+//    public String getCardYear() {
+//        return cardyear;
+//    }
+//
+//    private String cardcvv;
+//    public String getCardCVV() {
+//        return cardcvv;
+//    }
 
     private String name;
     public String getName() {
